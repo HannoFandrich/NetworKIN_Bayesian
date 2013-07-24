234bb81a572b4b546a454e5ea30fdd57871c7f26
Jinho Kim <jinhokim@cbs.dtu.dk>
2013-07-24 12:34:08 +0200

    add sample run script

---
+++ sample_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ~/Works/NetworKIN_Bayesian/NetworKIN.py -b /Users/jinhokim/Packages/blast-2.2.17/bin/blastall -n /Users/jinhokim/Works/NetPhorest_devel/NetPhorest_OUTPUT_v1.0/binary_20130617/netphorest -v -t 10 9606 sample.fas > NK_out_sample.txt
